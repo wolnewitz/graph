03d39c10303283f62d2cd70aadc309e59e9fa5c6
Cory Wolnewitz <wolnewitz@gmail.com>
2017-05-26 09:15:46 -0700

    addEdge

--- graph.js
+++ graph.js
@@ -1,12 +1,19 @@
 const Graph = function() {
   this.edgeList = [];
-  this.nodes = [];
+  this.vertices = [];
 };
 
-Graph.prototype.addVertex = (val) => {
+Graph.prototype.addVertex = function(val) {
+  this.vertices.push(val);
+  this.edgeList[val] = [];
 }
 
-Graph.prototype.removeVertex = (val) => {
+Graph.prototype.removeVertex = function() {
+}
+
+Graph.prototype.addEdge = function(vertex1, vertex2) {
+  this.edgeList[vertex1].push(vertex2);
+  this.edgeList[vertex2].push(vertex1);
 }
 
 module.exports = Graph
--- spec/graph.spec.js
+++ spec/graph.spec.js
@@ -8,15 +8,66 @@ describe('graph', () => {
     graph = new Graph();
   })
 
-  it('should exist', () => {
+  it('exists', () => {
     expect(graph).to.exist;
   });
 
-  it('should have an addVertex method', () => {
-    expect(graph.addVertex).to.exist;
+  describe('graph.addVertex', () => {
+    it('exist', () => {
+      expect(graph.addVertex).to.exist;
+    });
+
+    it('adds to vertices', () => {
+      graph.addVertex(1);
+      graph.addVertex(2);
+      graph.addVertex(3);
+      graph.addVertex(4);
+      expect(graph.vertices.length).to.equal(4);
+    });
+
+    it('adds an empty array to the edgeList at index of vertex', () => {
+      graph.addVertex(1);
+      graph.addVertex(2);
+
+      expect(graph.edgeList[1]).to.be.an('array').that.has.lengthOf(0);
+    });
   });
 
-  it('should have an removeVertex method', () => {
-    expect(graph.removeVertex).to.exist;
-  });
+  describe('graph.removeVertex', () => {
+    beforeEach(() => {
+      graph = new Graph();
+    })
+
+    it('has an removeVertex method', () => {
+      expect(graph.removeVertex).to.exist;
+    });
+  })
+
+  describe('graph.addEdge', () => {
+    beforeEach(() => {
+      graph = new Graph();
+    })
+
+    it('exists', () => {
+      expect(graph.addEdge).to.exist;
+    });
+
+    it('works', () => {
+      graph.addVertex(1)
+      graph.addVertex(2)
+      graph.addVertex(3)
+      graph.addVertex(4)
+
+      graph.addEdge(1, 3)
+      graph.addEdge(2, 4)
+      graph.addEdge(2, 1)
+
+      expect(graph.edgeList[1]).to.include(3);
+      expect(graph.edgeList[2].length).to.equal(2);
+      expect(graph.edgeList[2]).to.include(4, 1);
+      expect(graph.edgeList[4]).to.include(2);
+      expect(graph.edgeList[3]).to.include(1);
+      expect(graph.edgeList[1].length).to.equal(2);
+    });
+  })
 })
